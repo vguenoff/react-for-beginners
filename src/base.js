98178a13f2c37a87b8b359c0436b797ae630dace
Vasil Genov <vasil.genov@ad.mentormate.bg>
2018-03-01 19:16:33 +0200

    course restart march 2018

--- src/base.js
+++
@@ -1,9 +0,0 @@
-import Rebase from 're-base';
-
-const base = Rebase.createClass({
-  apiKey: 'AIzaSyBB0wCRIOgt0yUhgeVYvXpJEKx9LFcpfhE',
-  authDomain: 'fishie-fish.firebaseapp.com',
-  databaseURL: 'https://fishie-fish.firebaseio.com',
-});
-
-export default base;
--- src/components/NotFound.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NotFound = () => (
-  <h2>Ii|NotFoundI|I</h2>
-);
-
-export default NotFound;
