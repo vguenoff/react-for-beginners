3f459e2f56334dacf46af7a144f79e79191472be
vguenoff <vgenoff@gmail.com>
2018-03-02 10:54:32 +0200

    a

---
+++ catch-of-the-day/src/components/Inventory.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Inventory extends Component {
+  render() {
+    return (
+      <div className="inventory">Inventory</div>
+    )
+  }
+}
+
+export default Inventory;
---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Order extends Component {
+  render() {
+    return (
+      <div className="order">order</div>
+    )
+  }
+}
+
+export default Order;
